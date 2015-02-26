ad80cd4607452d6f0ec28583daff5150153c8e85
Diogo Gonçalves <diogomachadogoncalves@gmail.com>
2015-02-26 13:19:47 -0300

    [SRC] fix initial vertex

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 int main(){
 
     int n_ver, n_edge, i, from, to, cost, u=INF;
-    vEB *V = NULL;
+    vEB *veb = NULL;
     char opt;
 
     scanf("%d%d", &n_ver, &n_edge);
     vertex *vertices = (vertex*)calloc(n_ver, sizeof(vertex));
     initGraph(vertices, n_ver);
-    V = vEB_tree_insert(V, 0, 1, u);
+    veb = vEB_tree_insert(veb, 0, 0, u);
     for(i=0; i<n_edge; i++){
         scanf("%d%d%d", &from, &to, &cost);
         insertEdge(vertices, from, to, cost);
     }
-    dijkstra(vertices, &V, u);
+    dijkstra(vertices, &veb, u);
     for (i=0; i<n_ver; i++) {
         if (vertices[i].cost != INF)
             printf("%d\n", vertices[i].cost);
